f89cd35564f11b4ab93137f7ed3f78471eb85422
Arun_DEV <arunkumarroy.cse2021@gmail.com>
2025-10-24 00:47:12 +0600

    solve

--- src/hooks/axiousPublic.js
+++ src/hooks/axiousPublic.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiousPublic = axios.create({
   baseURL: "https://agency-server-five.vercel.app",
-  //withCredentials: true,
+  withCredentials: true,
 });
 
 export default axiousPublic;
