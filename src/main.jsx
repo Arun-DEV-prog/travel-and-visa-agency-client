eccc74abc33c74bdb49dc55b39fdbda19d7602cf
Arun_DEV <arunkumarroy.cse2021@gmail.com>
2025-10-23 23:48:15 +0600

    error fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
 import "./index.css";
 import App from "./App.jsx";
-import router from "./routes/Router.jsx";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./context/AuthProvider.jsx";
+import router from "./routes/router.jsx";
 
 const queryClient = new QueryClient();
 
